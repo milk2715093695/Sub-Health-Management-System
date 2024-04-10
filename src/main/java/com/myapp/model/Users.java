3d504ee031cf11378cc77827f09191ec28931d32
mac <laomms@outlook.com>
2024-04-11 00:46:09 +0800

    数据库代码

--- src/main/java/com/myapp/model/Users.java
+++ src/main/java/com/myapp/model/Users.java
@@ -1,7 +1,41 @@
 package com.myapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
+@Entity
 public class Users {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
