cea4c550b01e8a9d6de9c5ab5bceb7ba168ce411
mac <laomms@outlook.com>
2024-04-17 23:24:24 +0800

    前端->后段交互

---
+++ src/main/resources/static/js/checkUser.js
@@ -0,0 +1,24 @@
+document.getElementById("login-form").addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: document.getElementById('username').value,
+            password: document.getElementById('password').value
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                location.href = "/home";
+            } else {
+                alert('用户名或密码错误，请重新输入');
+                document.getElementById('username').value = '';
+                document.getElementById('password').value = '';
+            }
+        })
+})
