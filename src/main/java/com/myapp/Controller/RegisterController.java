507950a913ea5bb58981d7a96f9f812fe98f52cf
mac <laomms@outlook.com>
2024-04-18 15:37:03 +0800

    注册服务

--- src/main/java/com/myapp/Controller/RegisterController.java
+++ src/main/java/com/myapp/Controller/RegisterController.java
@@ -1,23 +1,25 @@
 package com.myapp.Controller;
 
 import com.myapp.Service.UserService;
+import com.myapp.model.UserData;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.HashMap;
 
-@Controller
+@RestController
 public class RegisterController {
     @Autowired UserService userService;
 
     @PostMapping("/register")
-    public Map<String, Object> register(@RequestParam("username") String username,
-                                        @RequestParam("password") String password) {
-
+    public Map<String, Object> register(@RequestBody UserData userData) {
         Map<String, Object> result = new HashMap<>();
+        String username = userData.getUsername();
+        String password = userData.getPassword();
         boolean success = userService.register(username, password);     //调用注册服务
 
         if (!success) {
