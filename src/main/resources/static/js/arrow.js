b2174dff75393d02825a0372e713180c572c9e0d
mac <laomms@outlook.com>
2024-04-22 21:33:37 +0800

    修改了已经废弃的符号

--- src/main/resources/static/js/arrow.js
+++ src/main/resources/static/js/arrow.js
@@ -1,16 +1,15 @@
 document.querySelector('.iBanner-arrow').addEventListener('click', function() {
     let text = document.querySelector('.wrapper p');
-    let a=document.querySelector('.iBanner-arrow');
+    let arrow=document.querySelector('.iBanner-arrow');
     if (text.style.display === 'none') {
         text.style.display = 'block';
-        a.style.display='none'
+        arrow.style.display='none'
     } else {
         text.style.display = 'none';
-
     }
     // 滚动到文本位置，smooth表示平滑地滚动
     window.scrollTo({
-        top: text.getBoundingClientRect().top + window.pageYOffset,
+        top: text.getBoundingClientRect().top + window.scrollY,
         behavior: 'smooth'
     });
 });
