378c7bbbdf280ac06253c7b44ff159eb1d78919b
mac <laomms@outlook.com>
2024-04-25 09:22:38 +0800

    homePage

--- src/main/resources/static/js/arrow.js
+++ src/main/resources/static/js/arrow.js
@@ -1,12 +1,10 @@
-document.querySelector('.iBanner-arrow').addEventListener('click', function() {
-    let text = document.querySelector('.wrapper p');
-    let arrow=document.querySelector('.iBanner-arrow');
-    if (text.style.display === 'none') {
-        text.style.display = 'block';
-        arrow.style.display='none'
-    } else {
-        text.style.display = 'none';
-    }
+document.getElementById('arrow').addEventListener('click', function() {
+    let text = document.getElementById('hidden-text');
+    let arrow= document.getElementById('arrow');
+
+    arrow.style.display = 'none';
+    text.style.display = 'block';
+
     // 滚动到文本位置，smooth表示平滑地滚动
     window.scrollTo({
         top: text.getBoundingClientRect().top + window.scrollY,
