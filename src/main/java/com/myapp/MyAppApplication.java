922ea67be8efd6a260b02a3d2afd9ea8c236cd91
mac <laomms@outlook.com>
2024-05-04 17:28:12 +0800

    None

--- src/main/java/com/myapp/MyAppApplication.java
+++ src/main/java/com/myapp/MyAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MyAppApplication.class, args);
     }
-
 }
