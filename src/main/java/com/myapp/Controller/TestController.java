c4fd400c664c94f2a13e22ce74e934ef4ec35157
mac <laomms@outlook.com>
2024-05-07 11:12:18 +0800

    Test

--- src/main/java/com/myapp/Controller/TestController.java
+++ src/main/java/com/myapp/Controller/TestController.java
@@ -1,14 +1,14 @@
 package com.myapp.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TestController {
     @GetMapping("/test")
-    public String test(RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("info", "这是一个测试网页");
+    public String test(Model model) {
+        model.addAttribute("info", "这是一个测试网页");
         return "test";
     }
 }
