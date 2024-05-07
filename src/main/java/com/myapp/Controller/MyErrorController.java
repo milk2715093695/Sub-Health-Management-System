4524e2e713c3f7701fbc97efbd286bbd1ac44d18
mac <laomms@outlook.com>
2024-05-07 10:56:25 +0800

    404网页

---
+++ src/main/java/com/myapp/Controller/MyErrorController.java
@@ -0,0 +1,13 @@
+package com.myapp.Controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+}
