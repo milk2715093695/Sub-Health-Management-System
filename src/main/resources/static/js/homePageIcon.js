36c7dd18d27670920f8de14a67e7e5f9870fe0b6
zhaojw <1661203511@qq.com>
2024-05-23 23:52:18 +0800

    homepage finish

---
+++ src/main/resources/static/js/homePageIcon.js
@@ -0,0 +1,19 @@
+var items1 = document.getElementsByClassName('ik-item');
+for(let i=0;i<items1.length;i++){
+    items1[i].addEventListener('mouseover',function (){
+       let items2 = document.getElementsByClassName('ik-bg');
+       let items3 = document.getElementsByClassName('btn1 btn-kepu');
+
+           items2[i].style.opacity = '1';
+           items3[i].style.opacity = '1';
+
+    });
+    items1[i].addEventListener('mouseout',function (){
+        let items2 = document.getElementsByClassName('ik-bg');
+        let items3 = document.getElementsByClassName('btn1 btn-kepu');
+
+            items2[i].style.opacity = '0';
+            items3[i].style.opacity = '0';
+
+    });
+}
