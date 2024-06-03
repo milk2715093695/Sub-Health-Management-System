cc9e7de6fb92661149ecff94e76b0d58c3b8436b
mac <laomms@outlook.com>
2024-06-03 19:04:08 +0800

    none

--- src/main/java/com/myapp/Controller/TestShowPdf.java
+++ src/main/java/com/myapp/Controller/TestShowPdf.java
@@ -9,9 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
