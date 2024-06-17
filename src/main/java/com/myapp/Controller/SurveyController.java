7add94f32af97413b71b055cd9061a889854b386
mac <laomms@outlook.com>
2024-06-17 15:10:32 +0800

    none

--- src/main/java/com/myapp/Controller/SurveyController.java
+++ src/main/java/com/myapp/Controller/SurveyController.java
@@ -2,9 +2,13 @@ package com.myapp.Controller;
 
 import com.myapp.Service.SurveyService;
 import com.myapp.entity.Survey;
+import com.myapp.entity.User;
 import com.myapp.model.HealthScore;
 import com.myapp.model.SurveyData;
+
+
 import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,8 +31,8 @@ public class SurveyController {
         System.out.println(healthScore.gender());
         System.out.println(healthScore.score());
         Map<String, Object> result = new HashMap<>();
-        String username = (String) session.getAttribute("username");
-        if (username == null || username.isEmpty()) {
+        User user = (User) session.getAttribute("user");
+        if (user == null || user.getUsername() == null || user.getUsername().isEmpty()) {
             Map<String, String> errMessage = new HashMap<>();
             errMessage.put("zh-CN", "你还未登录");
             errMessage.put("en", "You are not logged in");
@@ -36,7 +40,7 @@ public class SurveyController {
             result.put("success", false);
             return result;
         }
-        SurveyData surveyData = new SurveyData(username, healthScore.gender(), healthScore.score(), null, null);
+        SurveyData surveyData = new SurveyData(user.getUsername(), healthScore.gender(), healthScore.score(), null, null);
         Boolean success = surveyService.saveSurvey(new Survey(surveyData));
         result.put("success", success);
         if (!success) {
