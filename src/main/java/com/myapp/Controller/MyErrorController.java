6a445d696dafefaeb06406c556ebf17253cb254b
mac <laomms@outlook.com>
2024-06-27 23:13:50 +0800

    MyErrorController

--- src/main/java/com/myapp/Controller/MyErrorController.java
+++ src/main/java/com/myapp/Controller/MyErrorController.java
@@ -1,9 +1,14 @@
 package com.myapp.Controller;
 
+
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 错误路由处理的控制器
+ * @author milk
+ */
 @Controller
 public class MyErrorController implements ErrorController {
     /**
